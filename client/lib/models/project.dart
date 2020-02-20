76404020a9c0824f4154d0750ba71b6a0c73d531
MasfikNET <contact@masfik.net>
2020-02-20 17:54:23 +0000

    Added Project model and input_exception

---
+++ client/lib/models/project.dart
@@ -0,0 +1,38 @@
+import 'package:Tempo/models/task.dart';
+import 'package:Tempo/utils/input_exception.dart';
+
+class Project {
+  String _name;
+  DateTime _startDate;
+  DateTime _dueDate;
+  List<Task> _tasks;
+
+  Project(String name, DateTime startDate, DateTime dueDate) {
+    this.name = name;
+    this.startDate = startDate;
+    this.dueDate = dueDate;
+  }
+
+  String get name => _name;
+
+  DateTime get startDate => _startDate;
+
+  DateTime get dueDate => _dueDate;
+
+  set name(String value) {
+    if (value.isNotEmpty) this._name = value;
+    else throw InputException('Cannot create a project without a name!', 'name');
+  }
+
+  set startDate(DateTime date) {
+    if (date != null) this._startDate = date;
+    else throw InputException('Cannot create a project without a start date!', 'startDate');
+  }
+
+  set dueDate(DateTime date) {
+    if (date != null) this._dueDate = date;
+    else throw InputException('Cannot create a project without a due date!', 'dueDate');
+  }
+
+  addTask(Task task) => _tasks.add(task);
+}
---
+++ client/lib/utils/input_exception.dart
@@ -0,0 +1,3 @@
+class InputException extends ArgumentError {
+  InputException(String message, String name): super.value(null, message, name);
+}
