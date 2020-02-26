9ab6e02693d32e185ef04a16a1b92caaf71cc5c1
MasfikNET <contact@masfik.net>
2020-02-26 04:54:09 +0000

    Small changes and bug fixes * Changed SVG due to license attributions (FontAwesome) * Scan QR code page is now wrapped with a SingleChildScrollView to allow landscape mode or support small devices

--- client/lib/ui/pages/scan_qr.dart
+++ client/lib/ui/pages/scan_qr.dart
@@ -22,39 +22,41 @@ class _ScanQrScreenState extends State<ScanQrScreen> {
         backgroundColor: kTempoThemeData.canvasColor,
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SvgPicture.asset(
-              'images/qrcode-solid.svg',
-              color: Colors.white,
-              width: 200,
-            ),
-            SizedBox(height: 30),
-            Text(
-              'Scan QR Code',
-              style: kTextTitle,
-            ),
-            Text(
-              'Have an invitation from a colleague? 🤝\n'
-              'Scan their QR code to quickly add them.\n\n'
-              'You can also scan codes of a certain location and\n'
-              'mark your presence with no hassle. 📍',
-              textAlign: TextAlign.center,
-              style: kTextNormal,
-            ),
-            SizedBox(height: 50),
-            RaisedButton(
-              child: Text("Scan QR Code"),
-              color: kTempoThemeData.accentColor,
-              onPressed: () async {
-                if (await qr.scan())
-                  setState(() => isValid = true);
-                else
-                  setState(() => isValid = false);
-              }
-            ),
-          ],
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              SvgPicture.asset(
+                'images/qrcode.svg',
+                color: Colors.white,
+                width: 250,
+              ),
+              SizedBox(height: 10),
+              Text(
+                'Scan QR Code',
+                style: kTextTitle,
+              ),
+              Text(
+                'Have an invitation from a colleague? 🤝\n'
+                'Scan their QR code to quickly add them.\n\n'
+                'You can also scan codes of a certain location and\n'
+                'mark your presence with no hassle. 📍',
+                textAlign: TextAlign.center,
+                style: kTextNormal,
+              ),
+              SizedBox(height: 50),
+              RaisedButton(
+                child: Text("Scan QR Code"),
+                color: kTempoThemeData.accentColor,
+                onPressed: () async {
+                  if (await qr.scan())
+                    setState(() => isValid = true);
+                  else
+                    setState(() => isValid = false);
+                }
+              ),
+            ],
+          ),
         ),
       ),
     );
