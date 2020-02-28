2841a142249f65ddf7a776f3dc20276f2537da12
MasfikNET <contact@masfik.net>
2020-02-28 22:55:49 +0000

    Fixed InputException order

--- client/lib/utils/input_exception.dart
+++ client/lib/utils/input_exception.dart
@@ -1,3 +1,3 @@
 class InputException extends ArgumentError {
-  InputException(String message, String name): super.value(null, message, name);
+  InputException(String message, String name): super.value(null, name, message);
 }
