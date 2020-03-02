6c2f49d4457897c60b8e8283df48632d55af1204
MasfikNET <contact@masfik.net>
2020-03-02 20:23:48 +0000

    Added provider dependency for state management and minor code refactoring.

---
+++ client/lib/ui/tempo.dart
@@ -0,0 +1,23 @@
+import 'package:Tempo/ui/pages/add_project.dart';
+import 'package:Tempo/ui/pages/main_content.dart';
+import 'package:Tempo/ui/pages/scan_qr.dart';
+import 'package:Tempo/ui/pages/teams.dart';
+import 'package:Tempo/ui/style.dart';
+import 'package:flutter/material.dart';
+
+class Tempo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Tempo',
+      theme: kTempoThemeData,
+      initialRoute: '/',
+      routes: {
+        '/': (context) => MainContentScreen(),
+        '/addproject': (context) => AddProjectScreen(),
+        '/team': (context) => TeamsScreen(),
+        '/scan': (context) => ScanQrScreen()
+      },
+    );
+  }
+}
