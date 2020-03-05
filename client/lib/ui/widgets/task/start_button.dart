aa749bf93b378eaa2f8b53d7bd155a4f8099fd8b
MasfikNET <contact@masfik.net>
2020-03-05 01:22:06 +0000

    Added simple animation to the start button

--- client/lib/ui/widgets/task/start_button.dart
+++ client/lib/ui/widgets/task/start_button.dart
@@ -1,7 +1,6 @@
-import 'package:Tempo/ui/style.dart';
 import 'package:flutter/material.dart';
 
-class StartButton extends StatelessWidget {
+class StartButton extends StatefulWidget {
   final bool started;
   final Function onStart;
   final Function onStop;
@@ -15,15 +14,40 @@ class StartButton extends StatelessWidget {
   });
 
   @override
+  _StartButtonState createState() => _StartButtonState();
+}
+
+class _StartButtonState extends State<StartButton> with SingleTickerProviderStateMixin {
+  AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: const Duration(milliseconds: 200), vsync: this);
+    // When a new entry is added, the running tasks will move the animation forward (show the pause button instead)
+    if (widget.started) controller.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
+    // If the widget is disabled, reverse the animation for eventual previously-running tasks
+    if (!widget.enabled) controller.reverse();
     return IconButton(
-      color: kTempoThemeData.accentColor,
-      disabledColor: kTempoThemeData.disabledColor,
-      icon: Icon(started ? Icons.pause : Icons.play_arrow),
-      onPressed: enabled
+      icon: AnimatedIcon(
+        icon: AnimatedIcons.play_pause,
+        progress: controller,
+      ),
+      color: Theme.of(context).accentColor,
+      disabledColor: Theme.of(context).disabledColor,
+      onPressed: widget.enabled
           ? () {
-              if (!started) onStart();
-              else onStop();
+              if (!widget.started) {
+                widget.onStart();
+                controller.forward();
+              } else {
+                widget.onStop();
+                controller.reverse();
+              }
             }
           : null
     );
