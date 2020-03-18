3470ac2053aba3ec86f9aaa761bd167eed44070f
Tom <neystom@hotmail.de>
2020-03-18 16:18:10 +0000

    Changed sql scripts

--- service/tempo-db/src/main/resources/migrations/core/postgres/V003__room_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS ROOM (
-    room_id        SERIAL PRIMARY KEY,
-    building       VARCHAR(64) NOT NULL,
-    room           VARCHAR(64) NOT NULL   
-);
-
-INSERT INTO ROOM (building, room)
-VALUES ('Tower 42', 'Conference Room');
